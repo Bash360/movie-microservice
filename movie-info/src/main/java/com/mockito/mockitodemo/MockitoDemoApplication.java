c37d6c6941e9632b3d3bc2dc0a054c66bc4a8a42
mark bashir <beejayphil@gmail.com>
2020-01-22 08:38:22 +0100

    added eureka client to movie rating service

--- movie-info/src/main/java/com/mockito/mockitodemo/MockitoDemoApplication.java
+++ movie-info/src/main/java/com/mockito/mockitodemo/MockitoDemoApplication.java
@@ -2,12 +2,13 @@ package com.mockito.mockitodemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MockitoDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MockitoDemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MockitoDemoApplication.class, args);
+  }
 }
